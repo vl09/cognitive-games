99db03bc58543702bd9d283a14f08150b0b2aee4
vl09 <valentijn.leenaers@gmail.com>
2025-07-14 11:01:18 +0200

    Create wordlists.js

---
+++ word-game/wordlists.js
@@ -0,0 +1,32 @@
+// File: wordlists.js
+
+const wordLists = {
+  en: {
+    easy: [
+      /* All English words with 1-5 letters */
+      "ABILITY", "ABSOLUTE", "ACTION", "ACTIVE", "ADVICE", "AGENCY", "ANGER", "ANGLE", "ANIMAL", "APPLE", "AREA", "ARMY", "ARROW", "AWARD", "AXE" /* ...and so on */
+    ],
+    medium: [
+      /* All English words with 6-8 letters */
+      "ACADEMIC", "ACCIDENT", "ACCURACY", "ACCURATE", "ACROSS", "ADDITION", "ADEQUATE", "AIRPORT", "ALCOHOL", "ALMOST", "AMAZING", "AMBITION" /* ...and so on */
+    ],
+    hard: [
+      /* All English words with 9+ letters */
+      "ACHIEVEMENT", "ACQUISITION", "ADDITIONAL", "ADJUSTMENT", "ADMINISTRATION", "ADMIRATION", "ADVERTISING", "AGRICULTURE", "ALTERNATIVE", "ANNIVERSARY" /* ...and so on */
+    ]
+  },
+  nl: {
+    easy: [
+       /* All Dutch words with 1-5 letters */
+      "AAL", "AAS", "ACTE", "ADER", "ADVIES", "AKKER", "ALARM", "ALBUM", "AMBACHT", "ANGST", "ANKER", "AUTO", "AVOND", "BAAN", "BAARD", "BAAS" /* ...and so on */
+    ],
+    medium: [
+      /* All Dutch words with 6-8 letters */
+      "AAMBEELD", "AANDACHT", "AANDEEL", "AANGIFTE", "AANKOMST", "AANPAK", "AANVAL", "AANVRAAG", "ABDIJ", "ACTEUR", "ADRES", "AGENT" /* ...and so on */
+    ],
+    hard: [
+      /* All Dutch words with 9+ letters */
+      "AANBEVELING", "AANBIEDING", "AANDRIJVING", "AANDUIDING", "AARDAPPEL", "AARDBEVING", "AARDLEIDING", "AARDRIJKSKUNDE", "ABONNEMENT", "ACCEPTATIE", "ACCESSOIRE", "ADMINISTRATIE" /* ...and so on */
+    ]
+  }
+};
